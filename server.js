3fa278b4f01c9937560b7a2fafcd0fe08f12e586
GaneshSPatil <ganeshpl@thoughtworks.com>
2017-05-31 20:20:36 +0530

    Add morgan request logger for app

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var morgan = require('morgan')
+
 var app = express();
+app.use(morgan('combined'))
 
 app.post('/', function (req, res) {
   res.send('POST request to homepage');
