c70bca13533f1f9870ff182a828011c3700624e6
GaneshSPatil <ganeshpl@thoughtworks.com>
2017-05-31 20:41:46 +0530

    Add Basic authentication middleware

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
-var morgan = require('morgan')
+var morgan = require('morgan');
+const authenticate = require('./lib/authenticate');
 
 var app = express();
-app.use(morgan('combined'))
+
+app.use(morgan('combined'));
+app.use(authenticate);
 
 app.post('/', function (req, res) {
   res.send('POST request to homepage');
