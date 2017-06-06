bf416eb489f2850a81f36c94bd860bd91a8ba5c8
GaneshSPatil <ganeshpl@thoughtworks.com>
2017-06-06 09:56:39 +0530

    Add 'api/send_mail' endpoint to send mail

---
+++ configs/connectionArgs.js
@@ -0,0 +1,6 @@
+module.exports = {
+  user: process.env.ADMIN_ID || 'ganeshp8996@gmail.com',
+  password: process.env.ADMIN_PASSWORD || 'password',
+  host: 'smtp.gmail.com',
+  ssl: true
+};
--- server.js
+++ server.js
@@ -2,31 +2,28 @@ const  express = require('express');
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator');
 
-
 var morgan = require('morgan');
-const authenticate = require('./lib/authenticate');
+const path = require('path');
+const sendMail = require(path.resolve('lib/sendMail'));
+const authenticate = require(path.resolve('lib/authenticate'));
 
 var app = express();
 
 app.use(morgan('combined'));
 app.use(authenticate);
 app.use(bodyParser.json());
-app.use(expressValidator([]));
 
-app.post('/', function (req, res) {
-  req.checkBody({
-    'subject': {
-      notEmpty: true
-    }
-  });
+app.post('/api/send_mail', function (req, res) {
+  var mailConfig = {
+    text: req.body.content,
+    to: req.body.to.join(', '),
+    attachment: req.body.attachments || [],
+    subject: req.body.subject
+  };
 
-  req.getValidationResult().then(function(result) {
-    if (!result.isEmpty()) {
-     res.status(400).send('Validation Failed:\n' + JSON.stringify(result.array()));
-     return;
-    }
-    res.send('POST request to homepage');
-  });
+   sendMail(mailConfig)
+    .then((msg) => res.send(msg))
+    .catch((err) => res.status(400).send(err));
 });
 
 app.listen(4567);
